96c404f8208d6660cb7b71dd3120bc2c1c701d9f
Romain Sabathe <RSabathe@gmail.com>
2025-06-19 08:00:56 +0100

    nvim: deprecating some useless split navigation commands

--- dot_config/nvim/lua/user/core/executable_keymaps.lua
+++ dot_config/nvim/lua/user/core/executable_keymaps.lua
@@ -1,4 +1,4 @@
-vim.g.mapleader = " " -- setting space as leader key.
+vim.g.mapleader = " "     -- setting space as leader key.
 
 local keymap = vim.keymap -- shortcut
 
@@ -15,6 +15,8 @@ keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" })
 -- Making page up/down actually go half the height instead of whole height
 keymap.set({ "n", "v" }, "<PageUp>", "<C-u>", { desc = "Go half page up" })
 keymap.set({ "n", "v" }, "<PageDown>", "<C-d>", { desc = "Go half page up" })
+keymap.set({ "n", "v" }, "<C-up>", "<C-u>", { desc = "Go half page up" })
+keymap.set({ "n", "v" }, "<C-down>", "<C-d>", { desc = "Go half page up" })
 
 -- Use j/k to navigate wrapped lines (useful for prose)
 keymap.set("n", "j", "gj", { noremap = true })
@@ -46,16 +48,6 @@ keymap.set("n", "<S-Right>", "<cmd>tabn<CR>", { desc = "Go to next tab" })
 keymap.set("n", "<S-Left>", "<cmd>tabp<CR>", { desc = "Go to previous tab" })
 keymap.set("n", "<leader>Tf", "<cOpen new tabmd>tabnew %<CR>", { desc = "Open current buffer in new tab" })
 
--- -- splits navigation
--- keymap.set("n", "<C-Left>", "<C-w>h", { desc = "Go to left split" })
--- keymap.set("n", "<C-h>", "<C-w>h", { desc = "Go to left split" })
--- keymap.set("n", "<C-Right>", "<C-w>l", { desc = "Go to right split" })
--- keymap.set("n", "<C-l>", "<C-w>l", { desc = "Go to right split" })
--- keymap.set("n", "<C-Up>", "<C-w>k", { desc = "Go to upper split" })
--- keymap.set("n", "<C-k>", "<C-w>k", { desc = "Go to upper split" })
--- keymap.set("n", "<C-Down>", "<C-w>j", { desc = "Go to bottom split" })
--- keymap.set("n", "<C-j>", "<C-w>j", { desc = "Go to bottom split" })
-
 -- code formatting
 keymap.set("v", "<leader>F", "gq", { desc = "Format the seleted text" })
 keymap.set("n", "<leader>F", "gqq", { desc = "Format the current line" })
@@ -63,31 +55,21 @@ keymap.set("n", "<leader>F", "gqq", { desc = "Format the current line" })
 -- exit terminal mode
 keymap.set("t", "<Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
--- -- terminal splits navigation
--- keymap.set({ "t", "i" }, "<C-Left>", "<C-\\><C-N><C-w>h", { desc = "Go to left split" })
--- keymap.set({ "t", "i" }, "<C-h>", "<C-\\><C-N><C-w>h", { desc = "Go to left split" })
--- keymap.set({ "t", "i" }, "<C-Right>", "<C-\\><C-N><C-w>l", { desc = "Go to right split" })
--- keymap.set({ "t", "i" }, "<C-l>", "<C-\\><C-N><C-w>l", { desc = "Go to right split" })
--- keymap.set({ "t", "i" }, "<C-Up>", "<C-\\><C-N><C-w>k", { desc = "Go to upper split" })
--- keymap.set({ "t", "i" }, "<C-k>", "<C-\\><C-N><C-w>k", { desc = "Go to upper split" })
--- keymap.set({ "t", "i" }, "<C-Down>", "<C-\\><C-N><C-w>j", { desc = "Go to bottom split" })
--- keymap.set({ "t", "i" }, "<C-j>", "<C-\\><C-N><C-w>j", { desc = "Go to bottom split" })
-
 -- adding "dp" and "dg" (diffput and diffget) to work in visual mode
 local function setup_diff_mappings()
-	if vim.opt.diff:get() then
-		vim.keymap.set("n", "dp", "dp", { buffer = true })
-		vim.keymap.set("v", "dp", ":diffput<CR>", { buffer = true })
-		vim.keymap.set("n", "dg", "do", { buffer = true })
-		vim.keymap.set("v", "dg", ":diffget<CR>", { buffer = true })
-	end
+  if vim.opt.diff:get() then
+    vim.keymap.set("n", "dp", "dp", { buffer = true })
+    vim.keymap.set("v", "dp", ":diffput<CR>", { buffer = true })
+    vim.keymap.set("n", "dg", "do", { buffer = true })
+    vim.keymap.set("v", "dg", ":diffget<CR>", { buffer = true })
+  end
 end
 
 vim.api.nvim_create_autocmd("OptionSet", {
-	pattern = "diff",
-	callback = setup_diff_mappings,
+  pattern = "diff",
+  callback = setup_diff_mappings,
 })
 
 vim.api.nvim_create_autocmd("BufEnter", {
-	callback = setup_diff_mappings,
+  callback = setup_diff_mappings,
 })
--- dot_config/nvim/lua/user/plugins/splits/executable_smart-splits.lua
+++ dot_config/nvim/lua/user/plugins/splits/executable_smart-splits.lua
@@ -1,28 +1,28 @@
 return {
-	"mrjones2014/smart-splits.nvim",
-	config = function()
-		smart_splits = require("smart-splits")
-		keymap = vim.keymap
+  "mrjones2014/smart-splits.nvim",
+  config = function()
+    smart_splits = require("smart-splits")
+    keymap = vim.keymap
 
-		-- resizing splits
-		keymap.set("n", "<C-S-h>", smart_splits.resize_left)
-		keymap.set("n", "<C-S-Left>", smart_splits.resize_left)
-		keymap.set("n", "<C-S-j>", smart_splits.resize_down)
-		keymap.set("n", "<C-S-Down>", smart_splits.resize_down)
-		keymap.set("n", "<C-S-k>", smart_splits.resize_up)
-		keymap.set("n", "<C-S-Up>", smart_splits.resize_up)
-		keymap.set("n", "<C-S-l>", smart_splits.resize_right)
-		keymap.set("n", "<C-S-Right>", smart_splits.resize_right)
+    -- moving between splits
+    keymap.set("n", "<C-h>", smart_splits.move_cursor_left)
+    keymap.set("n", "<C-Left>", smart_splits.move_cursor_left)
+    keymap.set("n", "<C-j>", smart_splits.move_cursor_down)
+    keymap.set("n", "<C-Down>", smart_splits.move_cursor_down)
+    keymap.set("n", "<C-k>", smart_splits.move_cursor_up)
+    keymap.set("n", "<C-Up>", smart_splits.move_cursor_up)
+    keymap.set("n", "<C-l>", smart_splits.move_cursor_right)
+    keymap.set("n", "<C-Right>", smart_splits.move_cursor_right)
+    keymap.set("n", "<C-\\>", smart_splits.move_cursor_previous)
 
-		-- moving between splits
-		keymap.set("n", "<C-h>", smart_splits.move_cursor_left)
-		keymap.set("n", "<C-Left>", smart_splits.move_cursor_left)
-		keymap.set("n", "<C-j>", smart_splits.move_cursor_down)
-		keymap.set("n", "<C-Down>", smart_splits.move_cursor_down)
-		keymap.set("n", "<C-k>", smart_splits.move_cursor_up)
-		keymap.set("n", "<C-Up>", smart_splits.move_cursor_up)
-		keymap.set("n", "<C-l>", smart_splits.move_cursor_right)
-		keymap.set("n", "<C-Right>", smart_splits.move_cursor_right)
-		keymap.set("n", "<C-\\>", smart_splits.move_cursor_previous)
-	end,
+    -- resizing splits
+    keymap.set("n", "<C-S-h>", smart_splits.resize_left)
+    keymap.set("n", "<C-S-Left>", smart_splits.resize_left)
+    keymap.set("n", "<C-S-j>", smart_splits.resize_down)
+    keymap.set("n", "<C-S-Down>", smart_splits.resize_down)
+    keymap.set("n", "<C-S-k>", smart_splits.resize_up)
+    keymap.set("n", "<C-S-Up>", smart_splits.resize_up)
+    keymap.set("n", "<C-S-l>", smart_splits.resize_right)
+    keymap.set("n", "<C-S-Right>", smart_splits.resize_right)
+  end,
 }
