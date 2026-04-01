9c6f79f6a7e0f30a7a994e8272340303f14d44df
Romain Sabathe <RSabathe@gmail.com>
2026-04-01 07:19:09 +0100

    nvim: auto-open CodeDiff when inside a worktree

--- dot_config/nvim/lua/user/core/executable_init.lua
+++ dot_config/nvim/lua/user/core/executable_init.lua
@@ -1,2 +1,11 @@
 require("user.core.options")
 require("user.core.keymaps")
+
+-- Auto-open CodeDiff when inside a gwt worktree (~/worktrees/...)
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    if vim.fn.getcwd():match("/worktrees/") then
+      vim.schedule(function() vim.cmd("CodeDiff") end)
+    end
+  end,
+})
